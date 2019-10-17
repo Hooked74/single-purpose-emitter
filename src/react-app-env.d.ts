091ab4170365350286b7ddf15e8ed45514859f60
Новиков Игорь <novikovio74@gmail.com>
2019-10-18 01:41:16 +0500

    Update dependencies

--- src/react-app-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare namespace SinglePurposeEmitterCommon {
-  type int = number;
-  type Handler<T = any> = (...args: any[]) => T;
-}
-
-import H74_SPEC = SinglePurposeEmitterCommon;
